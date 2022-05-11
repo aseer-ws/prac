bc88e4a0a05f956203ea8d3e672dd36d22b31573
Aseer KT <aseer@wednesday.is>
2022-05-11 23:21:27 +0530

    commit 3

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use("/users", usersRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   console.log("abcdee");
+  console.log("change maded");
   next(createError(404));
 });
 
